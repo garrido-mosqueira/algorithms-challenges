f24bce28597005201b1bd956adb8ee6d611aac42
Francisco Garrido Mosqueira <garridof@ryanair.com>
2020-04-10 18:57:23 +0200

    Tree: Insertion of a Tree

---
+++ src/topic6_trees/Insertion.java
@@ -0,0 +1,56 @@
+package topic6_trees;
+
+public class Insertion {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int data) {
+        Node aux = root;
+
+        insertNode(aux, data);
+
+        return root;
+    }
+
+    private static void insertNode(final Node aux, final int data) {
+        if (aux == null) {
+            return;
+        }
+
+        if (data > aux.data) {
+            if (aux.right != null) {
+                insertNode(aux.right, data);
+            } else {
+                aux.right = new Node(data);
+            }
+        }else {
+            if (aux.left != null) {
+                insertNode(aux.left, data);
+            } else {
+                aux.left = new Node(data);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(4);
+
+        insert(root, 2);
+        insert(root, 7);
+        insert(root, 1);
+        insert(root, 3);
+
+        System.out.println("");
+
+    }
+
+}
