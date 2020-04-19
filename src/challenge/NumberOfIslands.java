91151afc39d08ace42ca81b4b6605df77461c868
Francisco Garrido Mosqueira <garridof@ryanair.com>
2020-04-19 11:46:45 +0200

    Graphs: Count Number of Islands - solution 2

--- src/challenge/NumberOfIslands.java
+++ src/challenge/NumberOfIslands.java
@@ -47,4 +47,32 @@ public class NumberOfIslands {
         System.out.println("total " + search);
 
     }
+
+    /**
+     * SOLUTION 2
+     */
+    public int numIslands2(char[][] grid) {
+
+        int count = 0, i, j;
+        for(i=0;i<grid.length;i++){
+            for(j=0;j<grid[i].length;j++){
+                if(grid[i][j] == '1'){
+                    count += 1;
+                    findIsland(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void findIsland(char[][]grid, int i, int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j] == '0'){
+            return;
+        }
+        grid[i][j] = '0';
+        findIsland(grid, i+1, j);
+        findIsland(grid, i-1, j);
+        findIsland(grid, i, j-1);
+        findIsland(grid, i, j+1);
+    }
 }
