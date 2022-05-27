049f1d5db743086b67069ddbf2549af12595c83a
fegm22 <garrido.mosqueira@gmail.com>
2022-05-27 08:21:15 +0200

    GroupAnagrams - great creative solution

---
+++ src/LeetCode/medium/GroupAnagrams.java
@@ -0,0 +1,90 @@
+package LeetCode.medium;
+
+import java.util.*;
+
+/*
+Group Anagrams:
+
+Write a function that takes in an array of strings and groups anagrams together.
+Anagrams are strings made up of exactly the same letters, where order doesn't matter. For example, "cinema" and
+"iceman" are anagrams; similarly, 'foo" and "ofo" are anagrams.
+
+Your function should return a list of anagram groups in no particular order.
+
+Sample Input
+words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
+
+Sample Output
+[["yo", "oy"], ["flop", "olfp"], ["act", "tac", "cat"], ["foo"]]
+
+ */
+public class GroupAnagrams {
+
+    public static void main(String[] args) {
+        testing(new String[]{"yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"}, "[[act, tac, cat], [olfp, flop] , [yo, oy], [foo]]");
+        testing(new String[]{"abc", "dabd", "bca", "cab", "ddba"}, "[[dabd, ddba], [abc, bca, cab]]");
+        testing(new String[]{"zxc", "asd", "weq", "sda", "qwe", "xcz"}, "[[asd, sda], [qwe, weq], [xcz, zxc]]");
+        testing(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}, "[[bat], [nat, tan], [ate, eat, tea]]");
+        testing(new String[]{"", ""}, "[[, ]]");
+        testing(new String[]{"", "b"}, "[[], [b]]");
+    }
+
+    private static void testing(String[] input, String expected) {
+        List<List<String>> lists = groupAnagrams(input);
+        System.out.println("Group Anagrams: " + lists);
+        System.out.println("Expected: " + expected);
+        System.out.println("-");
+    }
+
+    public static List<List<String>> groupAnagrams(String[] words) {
+        Map<String, List<String>> groupAnagrams = new HashMap<>();
+
+        for (String word : words) {
+            char[] character = word.toCharArray();
+            Arrays.sort(character);
+            String sorted = new String(character);
+
+            if (groupAnagrams.containsKey(sorted)) {
+                groupAnagrams.get(sorted).add(word);
+            } else {
+                groupAnagrams.put(sorted, new ArrayList<>(Arrays.asList(word)));
+            }
+        }
+
+        return new ArrayList<>(groupAnagrams.values());
+    }
+
+
+    private static boolean isAnagram(String s, String s1) {
+        if (s.length() != s1.length()) return false;
+        int[] ascii = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            ascii[s.charAt(i)]++;
+            ascii[s1.charAt(i)]--;
+        }
+
+        for (int i = 0; i < ascii.length; i++) {
+            if (ascii[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isAnagram1(String s, String s1) {
+        Map<Character, Integer> charCountMap = new HashMap<>();
+
+        for (char letter : s.toCharArray()) {
+            charCountMap.put(letter, charCountMap.getOrDefault(letter, 0) + 1);
+        }
+
+        for (char letter : s1.toCharArray()) {
+            charCountMap.computeIfPresent(letter, (key, value) -> --value);
+        }
+
+        return charCountMap.values().stream().allMatch(value -> value == 0);
+
+    }
+
+}
