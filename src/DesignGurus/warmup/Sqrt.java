f8fbadeb2252ebc05fc1be1a5af9501c81c2138e
san_fran <28862509+garrido-mosqueira@users.noreply.github.com>
2025-11-20 18:36:25 +0100

    Sqrt

--- src/DesignGurus/warmup/Sqrt.java
+++ src/DesignGurus/warmup/Sqrt.java
@@ -1,4 +1,25 @@
 package DesignGurus.warmup;
 
 public class Sqrt {
+
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        int start = 1, end = x;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid == x / mid) return mid;
+            else if (mid < x / mid) start = mid + 1;
+            else end = mid - 1;
+        }
+
+        return end;
+    }
+
+    static void main(String[] args) {
+        Sqrt solution = new Sqrt();
+        System.out.println(solution.mySqrt(16));
+        System.out.println(solution.mySqrt(8));
+    }
+
 }
