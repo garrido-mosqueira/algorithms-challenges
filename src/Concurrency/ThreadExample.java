5783035464dc7605377ba16b69b8e7015fe795e7
san_fran <28862509+garrido-mosqueira@users.noreply.github.com>
2025-12-29 16:37:00 +0100

    Playing with threads from zero.

--- src/Concurrency/ThreadExample.java
+++ src/Concurrency/ThreadExample.java
@@ -5,6 +5,7 @@ public class ThreadExample {
     public static class MyThread extends Thread {
         public void run() {
             System.out.println("MyThread run");
+            System.out.println("MyThread finished");
         }
     }
 
@@ -13,11 +14,20 @@ public class ThreadExample {
                 new Runnable() {
                     @Override
                     public void run() {
-                        System.out.println("This is a thread");
+                        System.out.println("Thread run");
+                        System.out.println("Thread finished");
                     }
                 }
         );
         thread.start();
+
+        Thread threadLambda = new Thread(
+                () -> {
+                    System.out.println("ThreadLambda run");
+                    System.out.println("ThreadLambda finished");
+                }
+        );
+        threadLambda.start();
 
         MyThread myThread = new MyThread();
         myThread.start();
