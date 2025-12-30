2b003be2b411beeca213f4ce646f084ea2d86011
san_fran <28862509+garrido-mosqueira@users.noreply.github.com>
2025-12-30 15:25:09 +0100

    RaceConditions - many null values

---
+++ src/Concurrency/RaceConditions.java
@@ -0,0 +1,32 @@
+package Concurrency;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class RaceConditions {
+
+    static void main() {
+        Map<String, String> sharedMap = new ConcurrentHashMap<>();
+        Thread thread1 = new Thread(getRunnable(sharedMap));
+        Thread thread2 = new Thread(getRunnable(sharedMap));
+        thread1.start();
+        thread2.start();
+    }
+
+    private static Runnable getRunnable(Map<String, String> sharedMap) {
+        return () -> {
+            for (int i = 0; i < 1_000_000; i++) {
+                if (sharedMap.containsKey("key")) {
+                    String val = sharedMap.remove("key");
+                    if (val == null) {
+                        System.out.println(
+                                "Iteration: " + i + ": Value for 'key' was null");
+                    }
+                } else {
+                    sharedMap.put("key", "value");
+                }
+            }
+        };
+    }
+
+}
